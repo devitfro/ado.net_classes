8bcd8b885db946ae2937feed01c309a70b1589fa
devitfro <alinabogdanovaaaaa@gmail.com>
2025-03-31 16:07:45 +0300

    update

---
+++ dapper_hw/dapper_hw/Models/ProductCategoryView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dapper_hw.Models;
+
+public partial class ProductCategoryView
+{
+    public int Id { get; set; }
+
+    public string product_name { get; set; } = null!;
+
+    public string category_name { get; set; } = null!;
+}
